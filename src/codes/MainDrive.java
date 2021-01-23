18c71101a45a62891f4cee7ae3f61eec1d8a8659
조경진 <cho881020@gmail.com>
2021-01-23 12:07:01 +0900

    1~45 범위의 값이 들어와야, 다음 숫자로 넘어가도록.

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -3,39 +3,54 @@ package codes;
 import java.util.Scanner;
 
 public class MainDrive {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 //		6개의 로또 숫자를 저장할 배열
 		int[] myLottoNums = new int[6];
-		
-		for (int i=0 ; i < myLottoNums.length ; i++) {
+
+		for (int i = 0; i < myLottoNums.length; i++) {
 			
-			System.out.print((i+1)+ "번째 로또 번호 입력 : ");
+//			for - 각각의 자리에 맞게 숫자를 대입하도록 하는 역할.
+//			올바른 숫자를 넣을때까지 반복.
 			
-			int inputNum = scan.nextInt();
-			
-			myLottoNums[i] = inputNum;
-			
+			while (true) {
+
+//				숫자 입력
+				System.out.print((i + 1) + "번째 로또 번호 입력 : ");
+				int inputNum = scan.nextInt();
+
+				//				입력해도 되는 숫자인지? 검사하고
+//				검사 1. 입력받은 숫자가 1 ~ 45 사이의 값인가?
+//				1 ~ 45 사이: 1보다는 크거나 같고, 동시에, 45보다는 작거나 같다.
+				boolean isRangeOk = (1 <= inputNum && inputNum <= 45);
+				
+//				그 결과가 OK일때만
+				if (isRangeOk) {
+
+//					입력받은 숫자를 배열에 저장
+					myLottoNums[i] = inputNum;
+					
+//					올바른 결과가 들어갔으면 => while문을 종료.
+//					다음 자리의 숫자를 받으러 이동.
+					break;
+				}
+				
+			}
+
 		}
-		
-		
+
 //		저장된 값들 출력
-		
+
 		for (int myNum : myLottoNums) {
 			System.out.println(myNum);
 		}
-		
-		
+
 	}
 
 }
-
-
-
-
 
 
 
