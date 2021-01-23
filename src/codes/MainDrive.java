32c2766bd1ed944ea285c1a78052092b0f093d30
조경진 <cho881020@gmail.com>
2021-01-23 11:41:04 +0900

    숫자 6개 입력 받기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,13 +1,37 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	
 	public static void main(String[] args) {
 		
-//		깃허브 연동 프로젝트
+		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("테스트 문장 출력");
+//		6개의 로또 숫자를 저장할 배열
+		int[] myLottoNums = new int[6];
+		
+		for (int i=0 ; i < myLottoNums.length ; i++) {
+			
+			System.out.print("로또 번호 입력 : ");
+			
+			int inputNum = scan.nextInt();
+			
+			myLottoNums[i] = inputNum;
+			
+		}
+		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
